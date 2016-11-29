027829acc85dc069f673fbf11cf87d57aa6b9a44
Daniel Brad <danieltbrad@gmail.com>
2016-11-28 20:46:06 -0800

    Refactor Phone model tests with FactoryGirl

--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -1,7 +1,18 @@
 FactoryGirl.define do
   factory :phone do
-    contact_id 1
-    phone "MyString"
-    phone_type "MyString"
+    association :contact
+    phone '123-555-1234'
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+
+    factory :work_phone do
+      phone_type 'work'
+    end
+
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
   end
 end
